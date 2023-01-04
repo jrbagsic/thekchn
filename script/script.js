a50e68b3247d7b64f7a42a57fd57d4059b3f8902
Junior Bagsic <junior.bagsic@tjene.com>
2023-01-04 17:22:29 -0500

    removing comments

--- script/script.js
+++ script/script.js
@@ -4,10 +4,8 @@ function toggleMobileMenu(menu) {
 }
 
 // SCROLL TO TOP
-// Get the button:
-let mybutton = document.getElementById('myBtn');
+let mybutton = document.getElementById('topBtn');
 
-// When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {
 	scrollFunction();
 };
@@ -21,8 +19,7 @@ function scrollFunction() {
 	}
 }
 
-// When the user clicks on the button, scroll to the top of the document
 function topFunction() {
-	document.body.scrollTop = 0; // For Safari
-	document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+	document.body.scrollTop = 0;
+	document.documentElement.scrollTop = 0;
 }
